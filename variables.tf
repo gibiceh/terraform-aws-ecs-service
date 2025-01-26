0e0b0e728dd20c6b777516200022c8d896e830c6
Geoff Hickman <geoff.hickmanii@gmail.com>
2025-01-26 13:17:56 -0600

    update to use AWS managed policy for ecs task execute role

--- variables.tf
+++ variables.tf
@@ -125,3 +125,8 @@ variable "byo_ecs_task_definition_arn" {
   default     = ""
 }
 
+variable "byo_ecs_task_execution_policy_arn" {
+  description = "This is the BYO (Bring Your Own) ECS task execution policy ARN for the application"
+  type        = string
+  default     = ""
+}
